0f913c51b8ce80e924b054a39094a380db6cd20e
vaibhav-khot <vaibhav.khot@neosofttech.com>
2016-11-18 19:20:52 +0530

    Displaying Array of Heroes

--- app/app.component.ts
+++ app/app.component.ts
@@ -6,17 +6,30 @@ export class Hero {
   id: number;
   name: string;
 }
+//Const HERO (caps)s
+const HEROS:Hero[] = [
+  {id : 1 ,name : 'Sagar' },
+  {id : 2 ,name : 'Aaron' },
+  {id : 3 ,name : 'Shyam' },
+  {id : 4 ,name : 'Bhupendra' },
+  {id : 5 ,name : 'Sharad' },
+  {id : 6 ,name : 'Rahul' },
+  {id : 7 ,name : 'Aniket' },
+  {id : 8 ,name : 'Vaibhav' }
+  // {id : 1 ,name : "" },
+  // {id : 1 ,name : "" },
+  // {id : 1 ,name : "" },
+  // {id : 1 ,name : "" },
+  //
+];
 
 @Component({
   selector: 'heroes-app',
   templateUrl: './app/template/herodetails.html'
 })
 
+
 export class AppComponent {
   title = 'Tour of Heroes';
-  // (set this) : (using this) { (with these properties)}
-  hero : Hero = {
-    id: 1,
-    name: 'VK'
-  };
+  hero = HEROS;
 }
