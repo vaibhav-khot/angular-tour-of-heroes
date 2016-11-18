be2c7afe7fc336f700796642810c0f3d615009e4
vaibhav-khot <vaibhav.khot@neosofttech.com>
2016-11-18 21:19:01 +0530

    (Click) handler + display Data + Two way Binding

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
 
-//Class
+//Class  : means Type Declaration  : (Type)
 export class Hero {
   id: number;
   name: string;
+
+
+
 
 }
 //Const HERO (caps)s
@@ -24,67 +27,24 @@ const HEROS:Hero[] = [
   //
 ];
 
+
+
 @Component({
   selector: 'heroes-app',
   templateUrl: './app/template/herodetails.html',
-  styles: [`
-  .selected {
-    background-color: #CFD8DC !important;
-    color: white;
-  }
-  .heroes {
-    margin: 0 0 2em 0;
-    list-style-type: none;
-    padding: 0;
-    width: 15em;
-  }
-  .heroes li {
-    cursor: pointer;
-    position: relative;
-    left: 0;
-    background-color: #EEE;
-    margin: .5em;
-    padding: .3em 0;
-    height: 1.6em;
-    border-radius: 4px;
-  }
-  .heroes li.selected:hover {
-    background-color: #BBD8DC !important;
-    color: white;
-  }
-  .heroes li:hover {
-    color: #607D8B;
-    background-color: #DDD;
-    left: .1em;
-  }
-  .heroes .text {
-    position: relative;
-    top: -3px;
-  }
-  .heroes .badge {
-    display: inline-block;
-    font-size: small;
-    color: white;
-    padding: 0.8em 0.7em 0 0.7em;
-    background-color: #607D8B;
-    line-height: 1em;
-    position: relative;
-    left: -1px;
-    top: -4px;
-    height: 1.8em;
-    margin-right: .8em;
-    border-radius: 4px 0 0 4px;
-  }
-`]
+  styleUrls:['./app/styles/style.css']
 
 })
 
-
+// (property) : (Type);
+// (property) = value;
 export class AppComponent {
   title = 'Tour of Heroes';
   heros = HEROS;
-  showHero = function(hero:Hero) {
-    alert("Hello from " + hero.name + " my ID is "+hero.id);
+  selectedHero: Hero ;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
-
+  // showHero = function(hero:Hero) {
+  //   alert("Hello from " + hero.name + " my ID is "+hero.id);
 }
