d977bfbd4109726e5cc73ed2ab1db7d854bb4d19
vaibhav-khot <vaibhav.khot@neosofttech.com>
2016-11-18 17:23:44 +0530

    Hero Details Show

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,22 @@
 import { Component } from '@angular/core';
 
+
+//Class
+export class Hero {
+  id: number;
+  name: string;
+}
+
 @Component({
-    selector: 'my-app',
-    template: `<h1>Hello Angular</h1>`
+  selector: 'heroes-app',
+  templateUrl: './app/template/herodetails.html'
 })
-export class AppComponent { }
+
+export class AppComponent {
+  title = 'Tour of Heroes';
+  // (set this) : (using this) { (with these properties)}
+  hero : Hero = {
+    id: 1,
+    name: 'VK'
+  };
+}
