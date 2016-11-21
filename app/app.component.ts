2f8e2aca17c17ed86a3c585da44b874528b2eeeb
vaibhav-khot <vaibhav.khot@neosofttech.com>
2016-11-21 19:44:08 +0530

    Services EC6(Promice) working EC5 need to SEE

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,55 +2,35 @@ import { Component,OnInit } from '@angular/core';
 import { Hero } from './hero';
 import { HeroService } from './hero.service';
 
-
-//Class  : means Type Declaration  : (Type)
-// export class Hero {
-//   id: number;
-//   name: string;
-// }
-
-
-//
-//
-//
-// //Const HERO (caps)s
-// const HEROS:Hero[] = [
-//   {id : 1 ,name : 'Sagar' },
-//   {id : 2 ,name : 'Aaron' },
-//   {id : 3 ,name : 'Shyam' },
-//   {id : 4 ,name : 'Bhupendra' },
-//   {id : 5 ,name : 'Sharad' },
-//   {id : 6 ,name : 'Rahul' },
-//   {id : 7 ,name : 'Aniket' },
-//   {id : 8 ,name : 'Vaibhav' }
-// ];
-//
-
-
-
-
 @Component({
   selector: 'heroes-app',
   //templateUrl: './app/template/herodetails.html',
   templateUrl: './app/template/herodetails1.html',
-  styleUrls:['./app/styles/style.css']
+  styleUrls:['./app/styles/style.css'],
+  providers : [HeroService]
 
 })
+export class AppComponent implements OnInit{
 
-// (property) : (Type);
-// (property) = value;
-export class AppComponent {
   title = 'Tour of Heroes';
-  // heros = HEROS;
-  heros: Hero[];
-  selectedHero: Hero ;
+  heroes :Hero[];
+  selectedHero:Hero;
+  constructor(private heroService : HeroService){}
 
-  //Function Expression
-  onSelect(hero: Hero): void {
+  ngOnInit():void {
+  this.getHeroes();
+  console.log("ngOnInit");
+  console.log(this);
+  }
+
+  getHeroes():void {
+    this.heroService.getHeroesSlowly().then(heroes => this.heroes = heroes);
+    console.log("getHeroes");
+    console.log(this);
+    }
+
+
+    onSelect(hero:Hero):void {
     this.selectedHero = hero;
   }
-  //Function Statement
-  // showHero = function(hero:Hero) {
-  //   alert("Hello from " + hero.name + " my ID is "+hero.id);
-
 }
--- app/hero.service.ts
+++ app/hero.service.ts
@@ -1,14 +1,20 @@
-
-
-
 import { Injectable } from '@angular/core';
-
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 
-@Injectable()
+@Injectable(
+
+)
+
 export class HeroService {
-  getHeroes(): Hero[] {
-    return HEROES;
+
+  //ECMA Script 6 Way
+  getHeroes(): Promise<Hero[]> {
+    return Promise.resolve(HEROES);
   }
+  getHeroesSlowly(): Promise<Hero[]> {
+  return new Promise<Hero[]>(resolve =>
+    setTimeout(resolve, 2000)) // delay 2 seconds
+    .then(() => this.getHeroes());
+}
 }
