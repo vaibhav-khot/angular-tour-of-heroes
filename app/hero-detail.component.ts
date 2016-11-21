662b5027abb17162d770db8ee8824e6ae8a83ea7
vaibhav-khot <vaibhav.khot@neosofttech.com>
2016-11-21 12:47:03 +0530

    MULTIPLE Component and Template

--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -4,17 +4,7 @@ import { Hero } from './hero';
 
 @Component({
   selector: 'my-hero-detail',
-  template: `
-    <div *ngIf="hero">
-      <h2>{{hero.name}} details!</h2>
-      <div><label>id: </label>{{hero.id}}</div>
-      <div>
-        <label>name: </label>
-        <input [(ngModel)]="hero.name" placeholder="name"/>
-      </div>
-    </div>
-  `
-
+  templateUrl : './app/template/herodetails3.html',
 })
 
 export class HeroDetailComponent {
