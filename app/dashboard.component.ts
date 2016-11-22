db9cc6cbe8c3f5581bf891d22e3ddf329a2c42f5
vaibhav-khot <vaibhav.khot@neosofttech.com>
2016-11-22 16:38:47 +0530

    Dashboard and Heroes TAB Working

--- app/dashboard.component.ts
+++ app/dashboard.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core'
+import { Component , OnInit } from '@angular/core'
+import { Hero } from './hero'
+import { HeroService } from './hero.service'
+
+
+var module :{id :string}
 
 @Component({
+moduleId: module.id,
 selector :'my-dashboard',
-templateUrl:'./app/template/dashboard.html'
+templateUrl:'./template/dashboard.component.html'
 
+  // moduleId: module.id,
+  // selector: 'my-dashboard',
+  // templateUrl: 'dashboard.component.html'
 })
-export class DashboardComponent{
 
 
+
+export class DashboardComponent implements OnInit{
+heroes :Hero [] =[]
+constructor (private heroService : HeroService ){
+
+}
+ngOnInit() {
+  console.log(this);
+this.heroService.getHeroes().then(heroes=>{
+  console.log(heroes);
+  this.heroes=heroes;
+});
+
+
+
+}
 
 }
